9c63746b99cec2f8b41b01900bd35f141614f3fe
whn7979 <11254009+whn7979@user.noreply.gitee.com>
2024-09-24 22:30:16 +0800

    test

--- src/views/ScreenPage1.vue
+++ src/views/ScreenPage1.vue
@@ -7,7 +7,7 @@
       <span class="logo">
         <img :src="logoSrc" alt="" />
       </span>
-      <span class="title">电商平台实时监控系统</span>
+      <span class="title">title1111</span>
       <div class="title-right">
         <img :src="themeSrc" class="qiehuan" @click="handleChangeTheme">
         <span class="datetime">2049-01-01 00:00:00</span>
@@ -20,7 +20,8 @@
           <Trend ref="trend"></Trend>
           <div class="resize">
             <!-- icon-compress-alt -->
-            <span @click="changeSize('trend')" :class="['iconfont', fullScreenStatus.trend ? 'icon-compress-alt' : 'icon-expand-alt']"></span>
+            <span @click="changeSize('trend')"
+              :class="['iconfont', fullScreenStatus.trend ? 'icon-compress-alt' : 'icon-expand-alt']"></span>
           </div>
         </div>
         <div id="left-bottom" :class="[fullScreenStatus.seller ? 'fullscreen' : '']">
@@ -28,7 +29,8 @@
           <Seller ref="seller"></Seller>
           <div class="resize">
             <!-- icon-compress-alt -->
-            <span @click="changeSize('seller')"  :class="['iconfont', fullScreenStatus.seller ? 'icon-compress-alt' : 'icon-expand-alt']"></span>
+            <span @click="changeSize('seller')"
+              :class="['iconfont', fullScreenStatus.seller ? 'icon-compress-alt' : 'icon-expand-alt']"></span>
           </div>
         </div>
       </section>
@@ -38,7 +40,8 @@
           <Map ref="map"></Map>
           <div class="resize">
             <!-- icon-compress-alt -->
-            <span @click="changeSize('map')"  :class="['iconfont', fullScreenStatus.map ? 'icon-compress-alt' : 'icon-expand-alt']"></span>
+            <span @click="changeSize('map')"
+              :class="['iconfont', fullScreenStatus.map ? 'icon-compress-alt' : 'icon-expand-alt']"></span>
           </div>
         </div>
         <div id="middle-bottom" :class="[fullScreenStatus.rank ? 'fullscreen' : '']">
@@ -46,7 +49,8 @@
           <Rank ref="rank"></Rank>
           <div class="resize">
             <!-- icon-compress-alt -->
-            <span @click="changeSize('rank')"  :class="['iconfont', fullScreenStatus.rank ? 'icon-compress-alt' : 'icon-expand-alt']"></span>
+            <span @click="changeSize('rank')"
+              :class="['iconfont', fullScreenStatus.rank ? 'icon-compress-alt' : 'icon-expand-alt']"></span>
           </div>
         </div>
       </section>
@@ -56,7 +60,8 @@
           <Hot ref="hot"></Hot>
           <div class="resize">
             <!-- icon-compress-alt -->
-            <span @click="changeSize('hot')"  :class="['iconfont', fullScreenStatus.hot ? 'icon-compress-alt' : 'icon-expand-alt']"></span>
+            <span @click="changeSize('hot')"
+              :class="['iconfont', fullScreenStatus.hot ? 'icon-compress-alt' : 'icon-expand-alt']"></span>
           </div>
         </div>
         <div id="right-bottom" :class="[fullScreenStatus.stock ? 'fullscreen' : '']">
@@ -64,7 +69,8 @@
           <Stock ref="stock"></Stock>
           <div class="resize">
             <!-- icon-compress-alt -->
-            <span @click="changeSize('stock')"  :class="['iconfont', fullScreenStatus.stock ? 'icon-compress-alt' : 'icon-expand-alt']"></span>
+            <span @click="changeSize('stock')"
+              :class="['iconfont', fullScreenStatus.stock ? 'icon-compress-alt' : 'icon-expand-alt']"></span>
           </div>
         </div>
       </section>
@@ -82,16 +88,16 @@ import Trend from '@/components/Trend.vue'
 import { mapState } from 'vuex'
 import { getThemeValue } from '@/utils/theme_utils'
 export default {
-  created () {
+  created() {
     // 注册接收到数据的回调函数
     this.$socket.registerCallBack('fullScreen', this.recvData)
     this.$socket.registerCallBack('themeChange', this.recvThemeChange)
   },
-  destroyed () {
+  destroyed() {
     this.$socket.unRegisterCallBack('fullScreen')
     this.$socket.unRegisterCallBack('themeChange')
   },
-  data () {
+  data() {
     return {
       // 定义每一个图表的全屏状态
       fullScreenStatus: {
@@ -105,7 +111,7 @@ export default {
     }
   },
   methods: {
-    changeSize (chartName) {
+    changeSize(chartName) {
       // 1.改变fullScreenStatus的数据
       // this.fullScreenStatus[chartName] = !this.fullScreenStatus[chartName]
       // 2.需要调用每一个图表组件的screenAdapter的方法
@@ -123,7 +129,7 @@ export default {
       })
     },
     // 接收到全屏数据之后的处理
-    recvData (data) {
+    recvData(data) {
       // 取出是哪一个图表需要进行切换
       const chartName = data.chartName
       // 取出, 切换成什么状态
@@ -133,7 +139,7 @@ export default {
         this.$refs[chartName].screenAdapter()
       })
     },
-    handleChangeTheme () {
+    handleChangeTheme() {
       // 修改VueX中数据
       // this.$store.commit('changeTheme')
       this.$socket.send({
@@ -143,7 +149,7 @@ export default {
         value: ''
       })
     },
-    recvThemeChange () {
+    recvThemeChange() {
       this.$store.commit('changeTheme')
     }
   },
@@ -156,16 +162,16 @@ export default {
     Trend
   },
   computed: {
-    logoSrc () {
+    logoSrc() {
       return '/static/img/' + getThemeValue(this.theme).logoSrc
     },
-    headerSrc () {
+    headerSrc() {
       return '/static/img/' + getThemeValue(this.theme).headerBorderSrc
     },
-    themeSrc () {
+    themeSrc() {
       return '/static/img/' + getThemeValue(this.theme).themeSrc
     },
-    containerStyle () {
+    containerStyle() {
       return {
         backgroundColor: getThemeValue(this.theme).backgroundColor,
         color: getThemeValue(this.theme).titleColor
@@ -178,7 +184,7 @@ export default {
 <style lang="less" scoped>
 // 全屏样式的定义
 .fullscreen {
-  position: fixed!important;
+  position: fixed !important;
   top: 0 !important;
   left: 0 !important;
   width: 100% !important;
@@ -195,16 +201,19 @@ export default {
   color: #fff;
   box-sizing: border-box;
 }
+
 .screen-header {
   width: 100%;
   height: 64px;
   font-size: 20px;
   position: relative;
-  > div {
-      img {
-        width: 100%;
-      }
+
+  >div {
+    img {
+      width: 100%;
     }
+  }
+
   .title {
     position: absolute;
     left: 50%;
@@ -212,62 +221,74 @@ export default {
     font-size: 20px;
     transform: translate(-50%, -50%);
   }
+
   .title-right {
     display: flex;
     align-items: center;
-    position:absolute;
+    position: absolute;
     right: 0px;
     top: 50%;
     transform: translateY(-80%);
   }
+
   .qiehuan {
     width: 28px;
     height: 21px;
     cursor: pointer;
   }
+
   .datetime {
     font-size: 15px;
     margin-left: 10px;
   }
+
   .logo {
     position: absolute;
     left: 0px;
     top: 50%;
     transform: translateY(-80%);
+
     img {
       height: 35px;
       width: 128px;
     }
   }
 }
+
 .screen-body {
   width: 100%;
   height: 100%;
   display: flex;
   margin-top: 10px;
+
   .screen-left {
     height: 100%;
     width: 27.6%;
+
     #left-top {
       height: 53%;
       position: relative;
     }
+
     #left-bottom {
       height: 31%;
       margin-top: 25px;
       position: relative;
     }
   }
+
   .screen-middle {
     height: 100%;
     width: 41.5%;
     margin-left: 1.6%;
     margin-right: 1.6%;
+
     #middle-top {
       width: 100%;
       height: 56%;
       position: relative;
     }
+
     #middle-bottom {
       margin-top: 25px;
       width: 100%;
@@ -275,13 +296,16 @@ export default {
       position: relative;
     }
   }
+
   .screen-right {
     height: 100%;
     width: 27.6%;
+
     #right-top {
       height: 46%;
       position: relative;
     }
+
     #right-bottom {
       height: 38%;
       margin-top: 25px;
@@ -289,10 +313,10 @@ export default {
     }
   }
 }
+
 .resize {
   position: absolute;
   right: 20px;
   top: 20px;
   cursor: pointer;
-}
-</style>
+}</style>
