353bd311bf5d67ca92862a76745ff9510c2455c4
Liuhy <824022935@qq.com>
2024-12-07 14:30:27 +0800

    测点数据第一页

--- src/components/SubTitle.vue
+++ src/components/SubTitle.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="sub-title">
     <div v-for="item in routerList" :key="item.title" @click="$router.push(item.path)">
-      {{ item.title }}</div>
+      <h1>{{ item.title }}</h1></div>
   </div>
 </template>
 
@@ -21,9 +21,20 @@ export default {
 
 <style lang="less">
 .sub-title {
-  width: 100%;
+  width: 700px;
+  margin-left: 600px;
   height: 40px;
   display: flex;
   justify-content: space-between;
+  margin-top: 20px;
+  h1 {
+    font-size: 20px;
+    font-weight: bold;
+    color: white;
+    cursor: pointer;
+    &:hover {
+      color: blue;
+    }
+  }
 }
 </style>
--- src/views/StatsAnalysis/PointData.vue
+++ src/views/StatsAnalysis/PointData.vue
@@ -1,11 +1,221 @@
 <template>
   <div class="point-data">
-    测点数据
+    <div class="option-container">
+      <div class="selected-option">
+        <el-form :inline="true" :model="formInline" class="demo-form-inline">
+          <el-form-item label="场站类型">
+            <el-select v-model="formInline.siteName" placeholder="场站类型"></el-select>
+          </el-form-item>
+          <el-form-item label="设备选择">
+          <el-select v-model="formInline.dev" placeholder="设备选择"></el-select>
+          </el-form-item>
+          <el-form-item label="数据类型">
+            <el-select  v-model="formInline.dataType" placeholder="数据类型"></el-select>
+          </el-form-item>
+          <el-form-item >
+            <el-button type="primary" @click="onSubmit">查询</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div class="right-option">
+        <div class="data-compare">
+          <div class="data-compare-picture"> <img src="../../assets/pictures/data-compare.svg" alt=""></div>
+          <div class="data-compare-text">数据对比</div>
+        </div>
+        <div class="data-display">
+          <div class="data-display-picture"><img src="../../assets/pictures/data-display.svg" alt=""></div>
+          <div class="data-display-text">数据显示</div>
+        </div>
+        <div class="data-export">
+          <div class="data-export-picture"><img src="../../assets/pictures/data-export.svg" alt=""></div>
+          <div class="data-export-text">数据导出</div>
+        </div>
+      </div>
+    </div>
+    <div class="chart-container">
+      <div class="data-table">
+        <el-table
+            ref="multipleTable"
+            :data="tableData"
+            tooltip-effect="dark"
+            style="width: 100%"
+            @selection-change="handleSelectionChange">
+          <el-table-column
+            type="selection"
+            width="100">
+          </el-table-column>
+          <el-table-column
+            label="序号"
+            type="index"
+            width="150">
+          </el-table-column>
+          <el-table-column
+            label="测点名"
+            width="250"
+            prop="pointName">
+          </el-table-column>
+          <el-table-column
+            prop="pointNo"
+            label="测点号"
+            width="250">
+          </el-table-column>
+          <el-table-column
+            prop="pointDesc"
+            label="测点描述"
+            width="400">
+          </el-table-column>
+          <el-table-column
+            prop="siteType"
+            label="场站类型"
+            width="250">
+          </el-table-column>
+          <el-table-column
+            prop="pointCode"
+            label="测点编码"
+            width="250">
+          </el-table-column>
+          <el-table-column
+            prop="notes"
+            label="备注"
+            width="250">
+          </el-table-column>
+          
+        </el-table>
+        
+      </div>
+      <div class="data-pagination">
+        <el-pagination
+            background
+            layout="prev, pager, next"
+            :total="1000" >
+          </el-pagination>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+export default {
+  name: 'PointData',
+  data() {
+    return {
+      formInline: {
+        siteName: '',
+        dev: '',
+        dataType: '',
+      },
+      tableData: [
+        {
+          pointName: '测点1',
+          pointNo: '1',
+          pointDesc: '测点1描述',
+          siteType: '场站类型1',
+          pointCode: '测点编码1',
+          notes: '备注1',
+        },
+        {
+          pointName: '测点2',
+          pointNo: '2',
+          pointDesc: '测点2描述',
+          siteType: '场站类型2',
+          pointCode: '测点编码2',
+          notes: '备注2',
+        },
+        {
+          pointName: '测点3',
+          pointNo: '3',
+          pointDesc: '测点3描述',
+          siteType: '场站类型3',
+          pointCode: '测点编码3',
+          notes: '备注3',
+        },
+        {
+          pointName: '测点4',
+          pointNo: '4',
+          pointDesc: '测点4描述',
+          siteType: '场站类型4',
+          pointCode: '测点编码4',
+          notes: '备注4',
+        },
+        {
+          pointName: '测点5',
+          pointNo: '5',
+          pointDesc: '测点5描述',
+          siteType: '场站类型5',
+          pointCode: '测点编码5',
+          notes: '备注5',
+        },
+      ],
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.formInline);
+    },
+  },
+};
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+.point-data {
+  margin-top: 25px;
+  .option-container {
+    display: flex;
+    background-color: aliceblue;
+    .selected-option {
+      margin-top: 20px;
+      margin-left: 20px;
+      display: flex;
+      padding: 0px;
+    }
+    .right-option {
+      margin-top: 20px;
+      margin-left: 600px;
+      padding: 0px;
+      display: flex;
+      justify-content: space-between;
+      color: black;
+      .data-compare {
+        padding: 10px;
+        border-radius: 5px;
+        cursor: pointer;
+        
+      }
+      .data-display {
+        
+        padding: 10px;
+        border-radius: 5px;
+        cursor: pointer;
+      }
+      .data-export {
+        
+        padding: 10px;
+        border-radius: 5px;
+        cursor: pointer;
+      }
+    }
+  }
+  .chart-container {
+    background-color: #fff;
+    margin-top: 0px;
+    height: 735px;
+    .data-table {
+      width: 100%;
+      height: 600px;
+      
+      
+      .el-table {
+        border: 1px solid #EBEEF5;
+        border-radius: 4px;
+        background-color: #fff;
+        width: 100%;
+      }
+    }
+    .data-pagination {
+      height: 100px;
+      
+      text-align: center;
+    }
+  }
+}
+</style>
